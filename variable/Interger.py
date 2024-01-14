2b2ff8b125c61246798be49f1947854801d28b5d
Devendra Sharma <146749164+Devendra2005Sharma@users.noreply.github.com>
2024-01-14 15:44:13 +0000

    commit

---
+++ variable/Interger.py
@@ -0,0 +1,9 @@
+# Integer is non-decimal value . it is used by int keyword in programming .
+"""hello 
+devendra 
+this is prashant kumar upadhyay"""
+
+a = int(input("Enter the integer : "))
+print(a)
+print(type(a))
+print(float(a))
